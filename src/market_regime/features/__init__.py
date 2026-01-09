0eda376ac9557d6176d8506920fb2d62c0ecc20d
Szymon Pawlowski <szy.pawlo@gmail.com>
2026-01-09 01:39:02 +0100

    Add feature builder for Markov switching model.

--- src/market_regime/features/__init__.py
+++ src/market_regime/features/__init__.py
@@ -3,5 +3,11 @@
 from .utils import calculate_log_returns
 from .image_features import ImageFeatureBuilder
 from .sharpe_features import SharpeFeatureBuilder
+from .markov_features import MarkovFeatureBuilder
 
-__all__ = ["calculate_log_returns", "ImageFeatureBuilder", "SharpeFeatureBuilder"]
+__all__ = [
+    "calculate_log_returns",
+    "ImageFeatureBuilder",
+    "SharpeFeatureBuilder",
+    "MarkovFeatureBuilder",
+]
