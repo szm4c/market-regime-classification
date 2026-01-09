1114699daf82083c94eaa066a14b1a50d3748725
Szymon Pawlowski <szy.pawlo@gmail.com>
2026-01-09 01:26:45 +0100

    Finish work on ARCH model. It's a dead end. Mean model predictions are too weak and it simply can't produce any prediction beyond 'sideways'.

--- src/market_regime/features/__init__.py
+++ src/market_regime/features/__init__.py
@@ -2,5 +2,6 @@
 
 from .utils import calculate_log_returns
 from .image_features import ImageFeatureBuilder
+from .sharpe_features import SharpeFeatureBuilder
 
-__all__ = ["calculate_log_returns", "ImageFeatureBuilder"]
+__all__ = ["calculate_log_returns", "ImageFeatureBuilder", "SharpeFeatureBuilder"]
